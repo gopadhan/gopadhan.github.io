1404fe12b8ff7e7f6149b65349ff7cfb457d6a8e
gopadhan <gopadhancontact@gmail.com>
2017-09-28 23:59:37 -0500

    Update Splash.js

--- gamejs/states/Splash.js
+++ gamejs/states/Splash.js
@@ -9,8 +9,8 @@ var Splash = function () {};
 Splash.prototype = {
 
   loadScripts: function () {
-    game.load.script('gamemenu','gamemenu.js');
-    game.load.script('gamestart', 'gamestart.js');
+    game.load.script('gamemenu','gamejs/states/gamemenu.js');
+    game.load.script('gamestart', 'gamejs/states/gamestart.js');
     game.load.script('common', 'gamejs/lib/common.js');
     //game.load.script('gameover','gamejs/states/GameOver.js');
     //game.load.script('options', 'gamejs/states/Options.js');
